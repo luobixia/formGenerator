9e48b65a9c3f2f51bf6c53b7d3ede6aed1da654b
luobixia <luobixia@cmcm.com>
2021-11-19 18:26:24 +0800

    fix：接入sass

---
+++ src/views/index/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+      <div class="left-board"></div>
+      <div class="center"></div>
+      <div class="right-board"></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+  },
+  watch: {
+  },
+  mounted() {
+   
+
+  },
+  methods: {
+  }
+}
+</script>
+
+<style lang='scss'>
+// @import '@/styles/home';
+</style>
